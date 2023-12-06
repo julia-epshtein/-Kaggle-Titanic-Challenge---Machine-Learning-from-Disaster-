e388db681ca754ce634cfc5418e0cd941904b3c2
Julia Epshtein <jepshtein03@gmail.com>
2023-12-06 17:11:08 -0500

    initialized files and methods

---
+++ hw9.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import jax
+from jax import grad
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+import sklearn
+
+datapath = "./"
+
+# Fix-shape universal approximator method (kernel method) 
+def run_universal_approximator_method():
+    x=2
+    
+# Neural network based on universal approximator method
+def run_neural_network_based_method():
+    x=2
+
+# Tree-based approach
+def run_tree_based_approach():
+    x=2
+
+# Main method
+def main():
+    run_universal_approximator_method()
+    run_neural_network_based_method()
+    run_tree_based_approach()
