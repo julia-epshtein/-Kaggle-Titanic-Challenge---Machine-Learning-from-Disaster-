6736b78d0b86d8838b8f942dc6b7c757911f2c0a
Julia Epshtein <jepshtein03@gmail.com>
2023-12-10 22:35:12 -0500

    Implemented Random Forest

--- hw9.py
+++ hw9.py
@@ -4,9 +4,17 @@ import jax
 from jax import grad
 import jax.numpy as jnp
 import matplotlib.pyplot as plt
-import sklearn
+from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split # Import train_test_split function
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
 
 datapath = "./"
+
+train_data = pd.read_csv('HW 9/-Kaggle-Titanic-Challenge---Machine-Learning-from-Disaster-/train.csv')
+test_data = pd.read_csv('HW 9/-Kaggle-Titanic-Challenge---Machine-Learning-from-Disaster-/test.csv')
+
+col_names = train_data.columns
 
 # Fix-shape universal approximator method (kernel method) 
 def run_universal_approximator_method():
@@ -16,12 +24,26 @@ def run_universal_approximator_method():
 def run_neural_network_based_method():
     x=2
 
+################## TASK 3 ##################
+
 # Tree-based approach
 def run_tree_based_approach():
-    x=2
+    # Obtain Training Set From Training Data and Testing Set From Testing Data
+    y = train_data["Survived"]
+
+    features = ["Pclass", "Sex", "SibSp", "Parch"]
+    X = pd.get_dummies(train_data[features])
+    X_test = pd.get_dummies(test_data[features])
+
+    model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+    model.fit(X, y)
+    predictions = model.predict(X_test)
+
+    output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+    output.to_csv('submission.csv', index=False)
 
 # Main method
-def main():
-    run_universal_approximator_method()
-    run_neural_network_based_method()
+if __name__ == '__main__':
+    #run_universal_approximator_method()
+    #run_neural_network_based_method()
     run_tree_based_approach()
