dc031c6239e8e4a38d7e499e9840f7057dc08314
Julia Epshtein <jepshtein03@gmail.com>
2023-12-11 14:09:54 -0500

    Implemented Universal Approximators

--- hw9.py
+++ hw9.py
@@ -11,22 +11,13 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression, Perceptron, SGDClassifier
-from sklearn.svm import SVC, LinearSVC
+from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
 
 # Deep learning libraries
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Input
-
-# JAX library for automatic differentiation
-import jax
-from jax import grad
-import jax.numpy as jnp
+from keras.layers import Dense, Dropout
 
 datapath = "./"
 
@@ -50,10 +41,9 @@ train_data['Has_Cabin'] = train_data['Cabin'].apply(lambda x: 0 if pd.isna(x) el
 
 # Drop Unnecessary Columns
 train_data.drop(['Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
-test_data.drop(['Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
 
 # Normalize/Scale Numerical Data (using Standard Normalization)
-train_data[['Age', 'Fare']] = (train_data[['Age', 'Fare']] - train_data[['Age', 'Fare']].mean()) / train_data[['Age', 'Fare']].std()
+train_data[['Age', 'Fare']] = StandardScaler().fit_transform(train_data[['Age', 'Fare']])
 
 # Verify Data Types
 train_data['Age'] = train_data['Age'].astype(float)
@@ -80,7 +70,7 @@ test_data['Has_Cabin'] = test_data['Cabin'].apply(lambda x: 0 if pd.isna(x) else
 test_data.drop(['Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
 
 # Normalize/Scale Numerical Data (using Min-Max scaling)
-test_data[['Age', 'Fare']] = (test_data[['Age', 'Fare']] - test_data[['Age', 'Fare']].min()) / (test_data[['Age', 'Fare']].max() - test_data[['Age', 'Fare']].min())
+test_data[['Age', 'Fare']] = StandardScaler().fit_transform(test_data[['Age', 'Fare']])
 
 # Verify Data Types
 test_data['Age'] = test_data['Age'].astype(float)
@@ -90,58 +80,59 @@ test_data['Age'] = test_data['Age'].astype(float)
 # Normalize the data with standard scaling
 
 # Fix-shape universal approximator method (kernel method) 
-def run_universal_approximator_method():    
-    x_train = train_data.drop(['Survived'], axis = 1)
+def run_universal_approximator_method():
+    x_train = train_data.drop(['Survived'], axis=1)
     y_train = train_data['Survived']
-    x_test = test_data.drop(['PassengerId'], axis = 1)
-    y_test = test_data['PassengerId']
+    x_test = test_data  # Assuming you want to use the entire test_data for predictions
     
+    scaler = StandardScaler()
+    x_train_scaled = scaler.fit_transform(x_train)
+    x_test_scaled = scaler.transform(x_test)
+
     svc = SVC()
-    svc.fit(x_train, y_train)
-    Y_pred = svc.predict(x_test)
-    acc_svc = round(svc.score(x_train, y_train) * 100, 2)
+    svc.fit(x_train_scaled, y_train)
+    Y_pred = svc.predict(x_test_scaled)
+    acc_svc = round(svc.score(x_train_scaled, y_train) * 100, 2)
     print(acc_svc)
-
-
+    
+    predictions = Y_pred
+    output = pd.DataFrame({'PassengerId': passenger_ids_test, 'Survived': predictions})
+    output.to_csv('submission_svc.csv', index=False)
 
 ################## TASK 2 ##################
 
 def run_neural_network_based_method():
-    x_train = train_data.drop(['Survived'], axis = 1)
+    x_train = train_data.drop(['Survived'], axis=1)
     y_train = train_data['Survived']
-    x_test = test_data.drop(['PassengerId'], axis = 1)
-        
+    x_test = test_data  # Assuming you want to use the entire test_data for predictions
+    
     model = Sequential()
-    model.add(Dense(units = 32, input_shape = (7,), activation = 'relu'))
-    model.add(Dense(units = 64, activation = 'relu', kernel_initializer = 'he_normal', use_bias = False))
+    model.add(Dense(units=32, input_shape=(x_train.shape[1],), activation='relu'))
+    model.add(Dense(units=64, activation='relu', kernel_initializer='he_normal', use_bias=False))
     model.add(tf.keras.layers.BatchNormalization())
-    model.add(Dense(units = 128, activation = 'relu',kernel_initializer = 'he_normal', use_bias = False))
+    model.add(Dense(units=128, activation='relu', kernel_initializer='he_normal', use_bias=False))
     model.add(Dropout(0.1))
-    model.add(Dense(units = 64, activation = 'relu',kernel_initializer = 'he_normal', use_bias = False))
+    model.add(Dense(units=64, activation='relu', kernel_initializer='he_normal', use_bias=False))
     model.add(Dropout(0.1))
-    model.add(Dense(units = 32, activation = 'relu'))
+    model.add(Dense(units=32, activation='relu'))
     model.add(Dropout(0.15))
-    model.add(Dense(units = 16, activation = 'relu'))
-    model.add(Dense(units = 8, activation = 'relu',kernel_initializer = 'he_normal', use_bias = False))
-    model.add(Dense(units =1 , activation = 'sigmoid'))
-    
-    model.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(), metrics=['acc'])    
-    model.fit(x_train, y_train, batch_size = 32, verbose = 2, epochs = 50)
-    
-    # predict = model.predict(x_test)
-    # #since we have use sigmoid activation function in output layer
-    # predict = (predict > 0.5).astype(int).ravel()
-    # print(predict)
-    
-    # y_pred_rand = (model.predict(x_train) > 0.5).astype(int)
-    # print('Accuracy : ', np.round(metrics.accuracy_score(y_train, y_pred_rand)*100,2))  
+    model.add(Dense(units=16, activation='relu'))
+    model.add(Dense(units=8, activation='relu', kernel_initializer='he_normal', use_bias=False))
+    model.add(Dense(units=1, activation='sigmoid'))
 
-    
+    model.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(), metrics=['acc'])
+    model.fit(x_train, y_train, batch_size=32, verbose=2, epochs=50)
+
+    # Predict
+    predictions = (model.predict(x_test) > 0.5).astype(int).ravel()
+    output = pd.DataFrame({'PassengerId': passenger_ids_test, 'Survived': predictions})
+    output.to_csv('submission_nn.csv', index=False)    
+
+
 ################## TASK 3 ##################
 
 def run_tree_based_approach():
-    
-    # Obtain Training Set From Training Data and Testing Set From Testing Data
+    # Obtain Training Set From Training Data
     y = train_data["Survived"]
 
     features = ["Pclass", "Sex", "SibSp", "Parch"]
@@ -165,10 +156,11 @@ def run_tree_based_approach():
     # Save Submission
     predictions = model.predict(X_test)
     output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
-    output.to_csv('submission.csv', index=False)
-    
+    output.to_csv('submission_trees.csv', index=False)
+
+
 # Main method
 if __name__ == '__main__':
     run_universal_approximator_method()
     #run_neural_network_based_method()
-    #run_tree_based_approach()
+    run_tree_based_approach()
